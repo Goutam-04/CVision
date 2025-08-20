a267c88c6e94b3e3e4384afd85d0df92628aaa63
Goutam-04 <goutamnayak2804+github1@gmail.com>
2025-08-20 14:04:22 +0530

    navbar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "CVision: AI powered resume analyzer" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Application & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powerd feedback</h2>
+
+      </div>
+
+    { resumes.length > 0 && (
+      <div className="resumes-section">
+
+    </div>
+    )}
+
+    {
+      resumes.map((resume) => (
+        <ResumeCard key={resume.id}  resume={resume}/>
+      ))}
+      </section>
+  </main>;
 }
